49e0598fe44890b8c82d9237a7073839388dda77
urosch121 <48390968+urosch121@users.noreply.github.com>
2019-03-17 03:07:04 +0100

    Add files via upload

---
+++ src/main/java/com/Prekvalifikacije/ORMPrimer/App.java
@@ -0,0 +1,13 @@
+package com.Prekvalifikacije.ORMPrimer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/Prekvalifikacije/ORMPrimer/Book.java
@@ -0,0 +1,43 @@
+package com.Prekvalifikacije.ORMPrimer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book 
+{
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String author;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
+	
+	
+
+}
